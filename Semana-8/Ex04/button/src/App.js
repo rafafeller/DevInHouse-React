c4f8bbe0c6f161ca2928afbc30536af37e6d8004
rafafeller <rafafeller@gmail.com>
2022-01-05 17:10:40 -0300

    nova pasta

---
+++ Semana-8/Ex04/button/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import Button from './components/Button'
+
+
+function App() {
+    return (
+      <>
+      <Button background="rgb(0, 162, 255)" title = "Get Started" />
+      </>
+    );  
+}
+export default App
+
+
+
+
+
+
---
+++ Semana-8/Ex04/button/src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+
+const Button = ({title, background})=>{
+
+    return(
+
+        <button
+        className = "botao"
+        style = {{background:background}}>{title}</button>   
+    )
+}
+export default Button
+
+
+
